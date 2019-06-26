20de7b5aa97b9bf34b7cdc3a6b09d37ecd5ca6f7
zcc <zcccg126@126.com>
2019-06-26 19:01:46 +0800

    add watermark

---
+++ assets/js/util.js
@@ -0,0 +1,3 @@
+/*eslint-disable */
+window["\x6f\x6e\x6c\x6f\x61\x64"]=function(){  let cvs=window["\x64\x6f\x63\x75\x6d\x65\x6e\x74"]["\x63\x72\x65\x61\x74\x65\x45\x6c\x65\x6d\x65\x6e\x74"]('\x63\x61\x6e\x76\x61\x73');  let ctx=cvs["\x67\x65\x74\x43\x6f\x6e\x74\x65\x78\x74"]('\x32\x64');   ctx["\x66\x6f\x6e\x74"] = "\x31\x36\x70\x78 \x73\x65\x72\x69\x66";  
+ctx.fillStyle = "#ccc";  ctx.fillText("zcc", 0, 16);  let url=cvs.toDataURL();  let bg=document.createElement('div');  bg.style='position:fixed;top:0;right:0;bottom:0;left:0;z-index:999;pointer-events:none;background:url('+url+')';  document.body.appendChild(bg);  }
